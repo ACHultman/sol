bc753df9a3b8bf5e298e981dc618d0b0543121ba
ACHultman <ahultman0@gmail.com>
2021-05-06 18:33:19 -0700

    feat: canvas, camera

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import "./App.css";
 import Sol from "./components/solar-system";
 import { Canvas } from "react-three-fiber";
+import React from "react";
+import "./app.css";
 
 function App() {
   return (
-    <div className="App">
-      <Canvas>
+    <div className="app">
+      <Canvas style={{ backgroundColor: "black" }} camera={[40, 0, 0]}>
         <Sol />
       </Canvas>
     </div>
